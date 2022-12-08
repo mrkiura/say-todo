4bcb408d62cdd8e5bc59f1bbfeeb683b152a4d62
Alex Kiura <kiuraalex@gmail.com>
2022-12-08 09:40:24 +0300

    Add header component

---
+++ src/features/todos/Header.tsx
@@ -0,0 +1,23 @@
+const Header = () => {
+    return (
+      <div className="bg-white">
+        <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            {/* <h2 className="text-lg font-semibold text-indigo-600">Pricing</h2> */}
+            <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
+              TODO
+            </p>
+            <div className="mx-auto mt-5 max-w-xl">
+
+            <p className=" text-xl text-gray-500">
+            Seize the day. Say bye bye to procrastination forever 💪
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
+export default Header
