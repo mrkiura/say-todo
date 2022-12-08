2ab6ce85f6b0d549263b71a303b1fde9ea12c203
Alex Kiura <kiuraalex@gmail.com>
2022-12-08 09:40:12 +0300

    Build footer component

---
+++ src/features/todos/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () =>  {
+    return (
+        <footer className="bg-white">
+        <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
+            <p className="text-center text-xl text-gray-500">
+                Motivation gets you started. Habit is what keeps you going.
+            </p>
+            <h2 className="text-sm font-semibold text-indigo-600 text-center">~ Jim Ryun</h2>
+          <p className="mt-8 text-center text-base text-gray-400">Built with ❤️ by <a href="https://github.com/mrkiura" title="Freepik">@mrkiura</a></p>
+        </div>
+      </footer>
+    )
+  }
+
+export default Footer;
